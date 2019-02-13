6119a91d51db53d3c081af8ad3532dda8b432ceb
Julian Donovan <donovan21@up.edu>
2019-02-12 23:04:12 -0800

    Added comments to the grader

--- public/functions.js
+++ public/functions.js
@@ -1,4 +1,9 @@
 //Written by Andrew Tagawa on 2/11/19
+/*Please note the client side scripting is currently intended to only support navigation of
+a mocked up scheduling app. Much of this sprint's scripting efforts went into the web scraper
+as we first need to gather data before our back-end can do any interesting database queries 
+or our front end may display any meaningful metrics
+*/
 
 function submit() {
 	//Dummied up function to trigger (currently empty) form submit
