8184a10efb1a3d230327b11a0d2b16239d35540d
Julian Donovan <donovan21@up.edu>
2019-02-25 01:39:24 -0800

    Simplified travis-ci

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const os = require('os');
+const mysql = require('mysql');
+
+//MySQL Setup
+sql = mysql.createConncetion({
+  host: '',
+  user: '',
+  password: '',
+  dtabase: ''
+});
+//sql.connect();
 
 const app = express();
 
 app.use(express.static('dist'));
 
-//app.get('/api/getSchedules', (req, res) => res.send({ username: os.userInfo().username }));
+//Client Side Post
+app.get('/api/getSchedules', (req, res) => res.send({ username: os.userInfo().username }));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
