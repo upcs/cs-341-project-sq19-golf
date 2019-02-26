2043b306a54ad2bcc8282382e33bca6e8f067ce7
Michael Quach <35576334+Quach19@users.noreply.github.com>
2019-02-25 20:44:37 -0800

    Added features to settings

--- src/client/js/settings_display.jsx
+++ src/client/js/settings_display.jsx
@@ -5,11 +5,12 @@ import '../css/styles.css';
 export class SettingsContainer extends Component {
 	constructor(props) {
 		super(props);
-	}
-	
+		};
+  
 	render() {
 		return (
 		<section id="main">
+			<button onClick={() => {alert('UPSchedule is a convenient schedule planner created by students, for students.');}}>About</button>
 			<Link to="/">
 			<button id="quit" type="button">
 				Quit
