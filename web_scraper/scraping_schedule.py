ce4d5bc354b05b77941a5c6e45be9a0fb7c53004
Nico <campuzan21@up.edu>
2019-02-25 19:29:49 -0800

    web_scraper update: generate CSV file for every course given a term. Added codecoverage + pydocs format

--- web_scraper/scraping_schedule.py
+++ web_scraper/scraping_schedule.py
@@ -3,13 +3,12 @@
 
 #import libraries
 import requests
-from bs4 import BeautifulSoup
-
-
 
 #handles separation of each class details, such as: name, time, days, instr
 def scrapeClass(html):
-
+	"""
+		Given a chunk of HTML data containing info for a specific course, parse all of it and return a list of: name, time, days, instructor
+	"""
 	#Grab the first desired data about the class name/title
 	classes = html.split('<a href', 1)
 	classes = classes[1].split('">', 1)
@@ -17,6 +16,12 @@ def scrapeClass(html):
 	classes = classes[1].split('Instructors</th>', 1)
 
 	#grab the second desired data about the time alotted for the class
+	################################################################
+	## Some Courses have accessible schedules for some reason, therefore the code crashes when splitting
+	## We'll choose to fill it in with "NULL" values -> our centinel value 
+	################################################################
+	if (len(classes) == 1):
+		return(["NULL", "NULL", "NULL", "NULL"])
 	time = classes[1].split('</td>', 1)
 	time = time[1].split('</td>', 1)
 	time = time[0].split('">')[1]
@@ -35,33 +40,36 @@ def scrapeClass(html):
 	instr = instr[1].split('</td>', 1)
 	instr = instr[1].split('(<ABBR', 1)
 	instr = instr[0].split('">')[1]
+	arr = ['', '', '', '']
+	arr[0] = name
+	arr[1] = time
+	arr[2] = days
+	arr[3] = instr
+	#return the desired data: name, time, days, instructor
+	return arr
 
-	#return the desired databits: name, time, days, instructor
-	return [name,time, days, instr]
 
+def getCourses(query):
+	"""
+		@query: the URL POST query to make to the UP dynamic schedule that returns all course for a major i.e. subject
+		Returns a two-dimensional list: list of courses, each course a list of info in string format
+	"""
+	#specify the url and get the HTML in string form.
+	schedule_page2 = requests.get(query).content.decode('utf-8')
+	#Keep only the HTML chunk with the info for every course
+	#splitting the html string after the tablerow once, then again, then once more after new search
+	myHTML = schedule_page2
+	s1 = myHTML.split('CLASS="ddtitle"', 1)
+	sFinal = s1[1].split('This is for formatting of the bottom links.', 1)
+	sFinal = sFinal[0]
+	sFinal = sFinal.split('CLASS="ddtitle"')
+	#above splitting will yield the section of data that contains the start of the
+	#	table data where the first class and info is listed, to the end of the
+	#	table data where the last class and info is listed.
 
+	#append each class' scraped data as an element of lstScrapedData
+	lstScrapedData = []
+	for x in sFinal:
+		lstScrapedData.append(scrapeClass(x))
+	return lstScrapedData
 
-#import urllib2
-
-#specify the url and get the HTML in string form.
-schedule_page2 = requests.get("https://selfserve-db.up.edu/prd/bwckschd.p_get_crse_unsec?term_in=201902&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=BIO&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a").content.decode('utf-8')
-
-#parse html using bs and store in variable 'soup'
-soup = BeautifulSoup(schedule_page2, 'html.parser')
-
-#splitting the html string after the tablerow once, then again, then once more after new search
-myHTML = schedule_page2
-s1 = myHTML.split('CLASS="ddtitle"', 1)
-sFinal = s1[1].split('This is for formatting of the bottom links.', 1)
-sFinal = sFinal[0]
-sFinal = sFinal.split('CLASS="ddtitle"')
-#above splitting will yield the section of data that contains the start of the
-#	table data where the first class and info is listed, to the end of the
-#	table data where the last class and info is listed.
-
-#append each class' scraped data as an element of lstScrapedData
-lstScrapedData = []
-for x in sFinal:
-	lstScrapedData.append(scrapeClass(x))
-#print (("\n").join(lstScrapedData))
-#Split the remaining of the tr
