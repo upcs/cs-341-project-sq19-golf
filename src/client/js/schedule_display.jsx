4fa530c645c4e7967bd1ca6de694639b57ec7e18
Julian Donovan <donovan21@up.edu>
2019-02-27 10:03:42 -0800

    Better unit tests

--- src/client/js/schedule_display.jsx
+++ src/client/js/schedule_display.jsx
@@ -3,75 +3,15 @@ import {Link} from 'react-router-dom';
 import { Schedules } from './input_display'
 import '../css/styles.css';
 
-//TODO: Remove this in favor of dynamic data
-const tmpSchedules = [
-  {
-    schedule: {
-      classes: [
-        {
-          startTime: '8:00am',
-          endTime: '9:30am',
-          courseName: 'CS 341',
-          profName: 'Nuxoll'
-        },
-        {
-          startTime: '9:30am',
-          endTime: '11:00am',
-          courseName: 'CS 349',
-          profName: 'Cenek'
-        },
-        {
-          startTime: '1:00pm',
-          endTime: '2:30am',
-          courseName: 'CS 321',
-          profName: 'Vegdahl'
-        }
-      ]
-    }
-  },
-  {
-    schedule: {
-      classes: [
-        {
-          startTime: '10:00am',
-          endTime: '11:30am',
-          courseName: 'CS 349',
-          profName: 'Cenek'
-        },
-        {
-          startTime: '11:30am',
-          endTime: '1:00pm',
-          courseName: 'CS 321',
-          profName: 'Vegdahl'
-        },
-        {
-          startTime: '4:00pm',
-          endTime: '6:30pm',
-          courseName: 'CS 341',
-          profName: 'Nuxoll'
-        }
-      ]
-    }
-  }
-];
-
 //All possible schedules
 export class SchedulesContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = { schedules: tmpSchedules };
-  }
-
-  //Retrieves generated schedule data
-  componentDidMount() {
-    fetch('/api/getSchedules')
-      .then(res => res.json())
-      .then(schedules => this.setState({ schedules: schedules }));
+    this.state = { schedules: props.schedules || [] };
   }
 
   render() {
-    let schedules = Schedules.viableSchedules;
-    console.log(schedules);
+    let schedules = Schedules.viableSchedules || this.state.schedules;
 
     let schedulesList = [];
     if (schedules) {
@@ -81,7 +21,6 @@ export class SchedulesContainer extends Component {
         )
       });
     }
-
 
     //TODO: Needs date/professor information
     return (
@@ -108,7 +47,7 @@ export class SchedulesContainer extends Component {
 }
 
 //A single schedule
-class ScheduleDisplay extends Component {
+export class ScheduleDisplay extends Component {
   constructor(props) {
     super(props);
     this.state = { schedule: props.schedule };
@@ -132,7 +71,7 @@ class ScheduleDisplay extends Component {
 };
 
 //A single course
-class ClassDisplay extends Component {
+export class ClassDisplay extends Component {
   constructor(props) {
     super(props);
     this.state = { classData: props.classData };
--- tests/schedule_display.test.jsx
+++ tests/schedule_display.test.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render, setState } from 'enzyme';
 
-import {SchedulesContainer} from '../src/client/js/schedule_display.jsx';
+import {SchedulesContainer, ScheduleDisplay, ClassDisplay} from '../src/client/js/schedule_display.jsx';
 
+//Mocked functions
+beforeEach(() => {
+});
+
+//Tests
 describe('SchedulesContainer', () => {
   test('Should render correctly', () => {
-    const schedulesContainer = shallow(<SchedulesContainer/>);
+    //Mocks class data state through providing abitrary properties
+    let schedules = [[], []];
+
+    const schedulesContainer = shallow(<SchedulesContainer schedules={schedules}/>);
     expect(schedulesContainer).toMatchSnapshot();
+  });
+});
+
+describe('ScheduleDisplay', () => {
+  test('Should render correctly', () => {
+    //Mocks class data state through providing abitrary properties
+    let schedule = [[], []];
+
+    const scheduleDisplay = shallow(<ScheduleDisplay schedule={schedule}/>);
+    expect(scheduleDisplay).toMatchSnapshot();
+  });
+});
+
+describe('ClassDisplay', () => {
+  test('Should render correctly', () => {
+    //Mocks class data state through providing abitrary properties
+    let classData = {'start': 0, 'end': 1, 'title': 'class'};
+
+    const classDisplay = shallow(<ClassDisplay classData={classData}/>);
+    expect(classDisplay).toMatchSnapshot();
   });
 });
