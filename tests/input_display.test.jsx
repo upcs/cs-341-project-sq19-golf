a361f69c5eed3315fe490b83d8dcfcb5a86b6cfa
Michael Quach <35576334+Quach19@users.noreply.github.com>
2019-02-28 21:17:12 -0800

    Update to Input_display_test

--- tests/input_display.test.jsx
+++ tests/input_display.test.jsx
@@ -14,6 +14,21 @@ describe('InputContainer', () => {
   test('Should render correctly', () => {
     const inputContainer = shallow(<InputContainer/>);
     expect(inputContainer).toMatchSnapshot();
+	
+	//Accepts necessary inputs
+	inputContainer.instance().necessaryInputs();
+    expect(inputContainer.instance().props.necessaryInputs).toBe(false);
+	
+	//Handle course input change
+    inputContainer.instance().handleCourseInputChange('inputID', 'courseID', 'subject');
+    expect(inputContainer.instance().props.handleCourseInputChange).toBe(false);
+
+    //Handle submission
+    inputContainer.instance().handleSubmit(event);
+    expect(inputContainer.instance().props.handleSubmit).toBe(false);
+
+    //The component should have reverted to its original state
+    expect(inputContainer).toMatchSnapshot();
   });
 });
 
@@ -21,6 +36,17 @@ describe('CourseInput', () => {
   test('Should render correctly', () => {
     const courseInput = shallow(<CourseInput/>);
     expect(courseInput).toMatchSnapshot();
+	
+	//Handle change
+    courseInput.instance().handleChange(event);
+    expect(courseInput.instance().props.handleChange).toBe(false);
+	
+	//Check if populated
+    courseInput.instance().checkIfPopulated(event);
+    expect(courseInput.instance().props.checkIfPopulated).toBe(false);
+	
+	//The component should have reverted to its original state
+	expect(inputContainer).toMatchSnapshot();
   });
 });
 
