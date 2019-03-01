76b7fcd80bb37d97329ab3fd012246cba43b2d48
tagawa21 <43159718+tagawa21@users.noreply.github.com>
2019-03-01 08:05:19 -0800

    results of preliminary test attempts

--- tests/sql.test.js
+++ tests/sql.test.js
@@ -1,11 +1,14 @@
 import Sequelize from 'sequelize';
 import { Classes } from '../src/server/sql.js';
-
+import { shallow } from 'enzyme';
+import Path  from 'path';
 //TODO: Fix this mockup
 
+
+/*
 describe('Classes model', () => {
   test('Loading a class object', () => {
-    /*Classes.create({
+    var x = Classes.create({
       subject: 'subject',
       number: 'number',
       title: 'title',
@@ -13,6 +16,21 @@ describe('Classes model', () => {
       start: 'start',
       end: 'end',
       days: 'days'
-    });*/
+    });
+  });
+});
+
+describe('Course data functions', () => {
+  test('Updating all course data', () => {
+
+  });
+  test('Updating all course data (ASync)', () => {
+
+  });
+  test('Getting all course data', () => {
+
+  });
+  test('Getting specific course data', () => {
+
   });
 });
