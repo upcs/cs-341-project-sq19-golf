b30cdc3421ef2a1bac6266295a4645047be4f949
Michael Quach <35576334+Quach19@users.noreply.github.com>
2019-03-24 14:51:07 -0700

    Fix to Packages

--- src/client/js/schedule_display.jsx
+++ src/client/js/schedule_display.jsx
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import html2canvas from 'html2canvas';
 import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 import ScrollMenu from 'react-horizontal-scrolling-menu';
 import {Link} from 'react-router-dom';
 import {connect} from "react-redux";
