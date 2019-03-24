b9fd963b5dd1ce283d9a0d082d5869a22b7b77ff
Michael Quach <35576334+Quach19@users.noreply.github.com>
2019-03-23 20:50:05 -0700

    Schedule Display Save PDF Button

--- src/client/js/schedule_display.jsx
+++ src/client/js/schedule_display.jsx
@@ -1,46 +1,71 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
 import {Link} from 'react-router-dom';
-import { Schedules } from './input_display'
+import {connect} from "react-redux";
+import {Schedules} from './input_display';
+import {store, modifySchedules} from './redux';
 import '../css/styles.css';
 
 //All possible schedules
 export class SchedulesContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = { schedules: props.schedules || [] };
+    this.connectSchedules = this.connectSchedules.bind(this);
+  }
+
+  connectSchedules() {
+    const mapStateToProps = state => {
+      return { viableSchedules: state.viableSchedules };
+    };
+
+    const schedulesList = ({ viableSchedules }) => {
+      return viableSchedules.map((schedule, i) => {
+        return <ScheduleDisplay key={"schedule-" + i} schedule={schedule}/>
+      });
+    };
+	
+    return connect(mapStateToProps)(schedulesList);
+  }
+  
+  printDocument() {
+    const input = document.getElementById('divToPrint');
+    html2canvas(input)
+      .then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, 'JPEG', 0, 0);
+        // pdf.output('dataurlnewwindow');
+        pdf.save("download.pdf");
+      })
+    ;
   }
 
   render() {
-    let schedules = Schedules.viableSchedules || this.state.schedules;
-
-    let schedulesList = [];
-    if (schedules) {
-      schedulesList = schedules.map((schedule, i) => {
-        return (
-          <ScheduleDisplay key={"schedule-" + i} schedule={schedule}/>
-        )
-      });
-    }
-
+    const Schedules = this.connectSchedules();
+	
     //TODO: Needs date/professor information
     return (
       <section id="main">
-		<div id="name">Schedule Name:
-			<input id="scheduleName" type="text" placeholder="Enter Schedule Name Here"/>
-		</div>
+    		<div id="name">Schedule Name:
+    			<input id="scheduleName" type="text" placeholder="Enter Schedule Name Here"/>
+    		</div>
         <div className="horiz-container">
-          {schedulesList}
+          <Schedules/>
         </div>
         <Link to="/">
           <button id="save" type="button">
             Save
           </button>
         </Link>
-		<Link to="/availability">
+		    <Link to="/availability">
           <button id="return" type="button">
             Return
           </button>
         </Link>
+		  <button onClick={this.printDocument}>Print</button>
       </section>
     );
   }
@@ -87,10 +112,88 @@ export class ClassDisplay extends Component {
         </span>
         <span className="classLabel">
           {classData.title}
-          {/*<br/>
-          {classData.profName} */}
+          <br/>
+          {classData.profName}
         </span>
       </div>
     );
   }
 };
+
+//Horizontal
+const list = [
+  { name: 'item1' },
+  { name: 'item2' },
+  { name: 'item3' },
+  { name: 'item4' },
+  { name: 'item5' },
+  { name: 'item6' },
+  { name: 'item7' },
+  { name: 'item8' },
+  { name: 'item9' }
+];
+
+const MenuItem = ({ text, selected }) => {
+  return (
+    <div
+      className="menu-item"
+    >
+      {text}
+    </div>
+  );
+};
+
+// All items component
+// Important! add unique key
+export const Menu = (list) => list.map(el => {
+  const { name } = el;
+
+  return (
+    <MenuItem
+      text={name}
+      key={name}
+    />
+  );
+});
+
+
+const Arrow = ({ text, className }) => {
+  return (
+    <div
+      className={className}
+    >{text}</div>
+  );
+};
+
+
+const ArrowLeft = Arrow({ text: '<', className: 'arrow-prev' });
+const ArrowRight = Arrow({ text: '>', className: 'arrow-next' });
+
+class App extends Component {
+  state = {
+    selected: 0
+  };
+  
+  onSelect = key => {
+    this.setState({ selected: key });
+  }
+
+  
+  render() {
+    const { selected } = this.state;
+    // Create menu from items
+    const menu = Menu(list, selected);
+
+    return (
+      <div className="App">
+        <ScrollMenu
+          data={menu}
+          arrowLeft={ArrowLeft}
+          arrowRight={ArrowRight}
+          selected={selected}
+          onSelect={this.onSelect}
+        />
+      </div>
+    );
+  }
+}
