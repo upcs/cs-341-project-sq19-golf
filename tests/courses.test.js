44bc0aa9de0ab2f6997ed16e7e5fbc3eac5b1c51
Michael Quach <35576334+Quach19@users.noreply.github.com>
2019-04-07 16:24:31 -0700

    Courses Test Update

--- tests/courses.test.js
+++ tests/courses.test.js
@@ -17,7 +17,15 @@ describe('Courses', () => {
 
     //The parsing script calls toUpper() on the title and day fields
     expect(testCourses[0].subject).toBe("TEST1");
-    expect(testCourses[2].days).toBe("DAYS3");
+	expect(testCourses[1].number).toEqual(0);
+    expect(testCourses[2].section).toBe("TEST2");
+	expect(testCourses[3].title).toBe("TEST2");
+	expect(testCourses[4].start).toBe("TEST2");
+	expect(testCourses[5].end).toBe("TEST2");
+	expect(testCourses[6].days).toBe("TEST2");
+	expect(testCourses[7].professor).toBe("TEST2");
+	expect(testCourses[8].location).toBe("TEST2");
+	expect(testCourses[9].credits).toBe("TEST2");
 
     //But not on the start field
     expect(testCourses[1].start).toBe("start2");
