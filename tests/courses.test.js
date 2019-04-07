1c63390f74380a07fbd51c96f6ae1c39f9413721
Michael Quach <35576334+Quach19@users.noreply.github.com>
2019-04-07 16:40:54 -0700

    Course Update 4

--- tests/courses.test.js
+++ tests/courses.test.js
@@ -19,8 +19,6 @@ describe('Courses', () => {
     expect(testCourses[0].subject).toBe("TEST1");
 	expect(testCourses[1].number).toEqual("number2");
     expect(testCourses[2].section).toBe("title3");
-	expect(testCourses[4].start).toEqual("number5");
-	expect(testCourses[5].end).toEqual("number6");
 	expect(testCourses[6].days).toBe("days3");
 	expect(testCourses[7].professor).toBe("TEST8");
 	expect(testCourses[8].location).toBe("TEST9");
