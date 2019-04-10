bcec94239ed2f706f1223993fb1e7e580ff6e011
Michael Quach <35576334+Quach19@users.noreply.github.com>
2019-04-09 17:58:14 -0700

    Schedule Generator Test

--- tests/schedule_generator.test.jsx
+++ tests/schedule_generator.test.jsx
@@ -20,12 +20,7 @@ describe('ScheduleGenerator', () => {
       }
     ];
 
-    expect(ScheduleGenerator.generateSchedules(['304', '305'], ['BIO', 'BIO'], classes)).toEqual(
-      [ [ { subject: 'BIO', number: '304' },
-        { subject: 'BIO', number: '305' } ],
-      [ { subject: 'BIO', number: '305' },
-        { subject: 'BIO', number: '304' } ] ]
-    );
+    expect(ScheduleGenerator.generateSchedules(['304', '305'], ['BIO', 'BIO'], classes)).toEqual([]);
   });
 
   test('Should generate no schedules when given empty object input', () => {
