a68963c2de7084349d252f76ef2c3c1c4ce31e4f
Michael Quach <35576334+Quach19@users.noreply.github.com>
2019-04-09 19:01:29 -0700

    Fail courses, but increase code cov

--- tests/courses.test.js
+++ tests/courses.test.js
@@ -12,7 +12,7 @@ describe('Courses', () => {
         test2,number2,title2,professor2,start2,end2,days2
         test3,number3,title3,professor3,start3,end3,days3*/
 
-    /* let dataPath = Path.join('tests', 'courses_test.csv');
+    let dataPath = Path.join('tests', 'courses_test.csv');
     var testCourses = Courses.parseCourseData(dataPath);
 
     //The parsing script calls toUpper() on the title and day fields
@@ -20,6 +20,6 @@ describe('Courses', () => {
     expect(testCourses[2].days).toBe("DAYS3");
 
     //But not on the start field
-    expect(testCourses[1].start).toBe("start2"); */
+    expect(testCourses[1].start).toBe("start2");
   });
 });
